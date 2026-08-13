6e79bbf92aa9f5dec219ca98f38da512a32ad1a6
joel-jain <joeljain04@gmail.com>
2026-08-13 09:24:51 +0530

    Refactor Teacher class and restore functionality

--- Assignment789/schoolmgmt.py
+++ Assignment789/schoolmgmt.py
@@ -1,25 +1,20 @@
-# from abc import ABC, abstractmethod
-# class Person(ABC):
-#     def __init__(self, T_id):
-#         self._id=T_id
-#     @abstractmethod
-#     def get_role_details(self):
-#         pass
-#     def display_id(self):
-#         print(f"T_id: {self._id}")
-# class Teacher(Person):
-#     def __init__(self, T_id,subject,department):
-#         super().__init__(T_id)
-#         self.subject=subject
-#         self.department=department
-#     def get_role_details(self):
-#         print(f"Subject:{self.subject}")
-#         print(f"Department:{self.department}")
-# teacher = Teacher(101,"eng","language")
-# teacher.display_id
-# teacher.get_role_details
-
- class Teacher(Person):
-    pass
-
+from abc import ABC, abstractmethod
+class Person(ABC):
+    def __init__(self, T_id):
+        self._id=T_id
+    @abstractmethod
+    def get_role_details(self):
+        pass
+    def display_id(self):
+        print(f"T_id: {self._id}")
+class Teacher(Person):
+    def __init__(self, T_id,subject,department):
+        super().__init__(T_id)
+        self.subject=subject
+        self.department=department
+    def get_role_details(self):
+        print(f"Subject:{self.subject}")
+        print(f"Department:{self.department}")
 teacher = Teacher(101,"eng","language")
+teacher.display_id
+teacher.get_role_details
